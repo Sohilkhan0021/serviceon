3901d199329938fc1acfd7197f82f6c80c7bfc4c
Sohil Khan <sohil0021khan@gmail.com>
2025-10-27 13:06:39 +0530

    fixed vite base path trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,52 @@
+// import { fileURLToPath, URL } from 'node:url';
+// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import tailwindcss from 'tailwindcss';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss()]
+//     }
+//   },
+//   base: '/serviceon',
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   build: {
+//     chunkSizeWarningLimit: 3000
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+
 import { fileURLToPath, URL } from 'node:url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tailwindcss from 'tailwindcss';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()]
     }
   },
-  base: '/serviceon',
+  
+  //  base changes automatically based on environment
+  base: mode === 'development' ? '/serviceon' : './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -19,4 +55,4 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit: 3000
   }
-});
+}));
