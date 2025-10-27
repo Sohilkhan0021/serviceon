1094d063c26c0dacf263456e2db3bbf9f9d42d41
Sohil Khan <sohil0021khan@gmail.com>
2025-10-27 13:26:51 +0530

    fixed react router basename for vercel routing

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
   return (
     <BrowserRouter
       // basename={BASE_URL}
-      basename={import.meta.env.DEV ? '/serviceon' : '/'}
+      basename={import.meta.env.DEV ? '/serviceon' : ''}
       future={{
         v7_relativeSplatPath: true,
         v7_startTransition: true
